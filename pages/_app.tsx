7314fdcceeb79b0c0185433e6c3fc7be80633cee
Audrey Li <me@audreyli.me>
2021-10-16 13:05:59 -0700

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faArrowUp,
+  faCar,
+  faCheckSquare,
+  faChild,
+  faCoffee,
+  faUniversalAccess,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+library.add(
+  faCoffee,
+  faCheckSquare,
+  faUser,
+  faChild,
+  faUniversalAccess,
+  faCar,
+  faArrowUp
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
